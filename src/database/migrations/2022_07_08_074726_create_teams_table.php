aaa6f0967f4eb5df479ac318d7ef9d2c07308dbe
fumiyamiyazaki <fmy9611@icloud.com>
2022-07-08 18:48:42 +0900

    feature create teams migration

---
+++ src/database/migrations/2022_07_08_074726_create_teams_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->increments('id')->length(4);
+            $table->string('name', 255);
+            $table->string('memo', 255)->nullable()->default(Null);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+};
